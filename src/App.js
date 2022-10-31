9034a3737f187387feba2b580cd7d84bbd8f27b2
amranhossain <amranhossainchy@gmail.com>
2022-10-31 21:42:26 +0600

    all working..>>history savings and local storage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form } from 'react-bootstrap';
+import { useState } from 'react';
+
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [tajbeehName, setTajbeehName] = useState('');
+  const [tajbeehNumber, setTajbeehNumber] = useState('');
+  // const [filled, setFilled] = useState(false);
+  const handleCount = () => {
+    setCount(count + 1);
+
+  }
+  const hanldeFormSubmit = (event) => {
+    event.prevent.default();
+  }
+  const handleTajbeehName = (event) => {
+    const inputName = event.target.value;
+    setTajbeehName(inputName);
+
+  }
+  const handleTajbeehNumber = (event) => {
+    const inputNumber = event.target.value;
+    setTajbeehNumber(inputNumber);
+  }
+
+  const handleReset = () => {
+    setCount(0);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className='my-5'> <span className='text-success'>Tajbeeh</span> Reader </h2>
+      <Form onSubmit={hanldeFormSubmit} className='w-50 mx-auto'>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control onBlur={handleTajbeehName} type="text" placeholder="Tasbeeh Name  " required />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control onChange={handleTajbeehNumber} type="number" placeholder="How Many time" required />
+        </Form.Group>
+        <div className='my-5'>
+          {
+            parseInt(tajbeehNumber) === count && (
+              <div className='d-flex align-items-center justify-content-center'>
+                <p>You did <span className='text-success fw-bold'>{tajbeehName}</span> {tajbeehNumber} times </p>
+                <Button className="mx-2" variant='success' size='sm'>Save</Button>
+              </div>
+            )
+          }
+          <h2> {count} </h2>
+          <Button onClick={handleCount}
+           disabled={(parseInt(tajbeehNumber) === count) || tajbeehNumber === ''}
+          className='mx-3'
+          variant="success" >Count</Button>
+          <Button  onClick={handleReset} variant="success" > Reset </Button>
+        </div>
+
+      </Form>
+      {/* History of tajbeeh reading */}
+      <div>
+          
+      </div>
+
     </div>
   );
 }
